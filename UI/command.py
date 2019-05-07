c0493a251fcfe66f1ad9ab5ea06b93c1788e5be7
kerimkilic <kerim40kilic@gmail.com>
2019-05-07 14:50:02 +0200

    Merged command_code and binary_command into one class

--- UI/command.py
+++ UI/command.py
@@ -31,16 +31,16 @@
 from tkinter import Label, Entry, Button, Tk
 
 import serial
+import crc8
 
 #ser = serial.Serial('/dev/ttyACM0', 9600, timeout=.5)
 
 #todo: toevoegen commandovertalen en CRC check
 
-
-class command_code: #commando vertalen van freq amp etc etc
+class command:
     command_type = {'frequency'     : [0x01,0],
                     'amplitude'     : [0x10,1],
-                    'keyfrequency'  : [0x20,1],
+                    'keyfrequency'  : [0x20,0],
                     'phaseshift'    : [0x30,1],
                     'info'          : [0x00,0],
                     'ping'          : [0xFF,0],
@@ -50,21 +50,24 @@ class command_code: #commando vertalen van freq amp etc etc
                     'gather'        : [0x40,1]
                     }
 
-    def __init__(self, command,  channel=0):
-        self.type = command_type[command]
+    def __init__(self, com, value=0, channel=0):
+        self.value = value
+        self.type = command.command_type[com]
         self.command = self.type[0]
         if self.type[1]:
             self.command += channel
 
-    def __call__(self):
-        return self.command
 
-class binaryCommand:
-    def __init__(self,command,value=None):
-        self.command = command
-        self.value = value
-    
-    def calcValue(self):
+    def __call__(self):
+        packet = list()
+        packet.append(ord("$"))
+        packet.append(self.command)
+        packet.extend(self.calc_value())
+        packet.append(self.crc(packet))
+        return packet
+
+
+    def calc_value(self):
         value = self.value
         result = list()
         if not value:
@@ -76,20 +79,9 @@ class binaryCommand:
         return result
 
     def crc(self,message):
-        return 0
-    def __call__(self):
-        packet = bytearray()
-        packet.append(ord("$"))
-        packet.append(self.command())
-        for i in self.calcValue():
-            packet.append(i)
-        packet.append(self.crc(packet))
-        return packet
+        hash = crc8.crc8()
+        hash.update(bytearray(message))
+        return int.from_bytes(hash.digest(),'little')
 
-InfoA = binaryCommand(command_code("frequency"), 1000) 
-#init functie
+InfoA = command("frequency", 1000) 
 print(InfoA())
-InfoB = binaryCommand(command_code("amplitude",0), 100)
-print(InfoB())
-#InfoC = binaryCommand(command_code("keyfrequency"), 62500)
-#print(InfoC())
