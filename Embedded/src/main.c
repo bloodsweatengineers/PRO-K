175812abab7bf468b68094ce68bf8723cd750c33
DennisWilPost <Denniswilpost@hotmail.nl>
2019-05-16 09:29:21 +0200

    test

--- Embedded/src/main.c
+++ Embedded/src/main.c
@@ -33,30 +33,106 @@ void main(void) {
 	TCCR0B = 0;
 	TCCR0B |= 1<<CS00;
 
+	typedef enum{
+		execute_state,
+		prepare_state,
+		start_state,
+		stop_state,
+	}system_state;
+
+	system_state next_state = stop_state;
+
+
 	sei();
 
 	while(1) {
 		struct token tok = parser();
-
-		if(tok.tok == REJECT) {
+		struct token new_event = tok.tok;
+		if(eNewEvent == REJECT){
 			uart_transmit_str("REJ\r\n");
-		} else if(tok.tok == FREQUENCY) {
-			frequency_conf(&conf, tok.value, -1);
-			frequency_execute(&conf);
-			uart_transmit_str("OK FREQUENCY\r\n");
-		} else if(tok.tok == PHASESHIFT) {
-			phaseshift_conf(&conf, tok.value, tok.channel);
-			phaseshift_execute(&conf);
-			char buffer[10];
-			memset(buffer, 0, 10);
-			int32_to_str(buffer, tok.channel);
-			uart_transmit_str("OK ");
-			uart_transmit_str(buffer);
-			uart_transmit_str(" PHASESHIFT\r\n");
-		} else if(tok.tok == AMPLITUDE) {
-			amplitude_conf(&conf, tok.value, tok.channel);
-			amplitude_execute(&conf);
-			uart_transmit_str("OK AMPLITUDE\r\n");
 		}
+		else if(eNewEvent == STOP){
+			eNextState = Stop();
+		}
+		else{
+			switch(eNextState){
+				case Stop_State:
+					if(eNewEvent == START){
+						eNextState = Start();
+					}				
+					break;
+				case Start_State:
+					if(eNewEvent == PREPARE){
+						eNextState = Prepare();
+					}
+					else if(eNewEvent == FREQUENCY){
+						Frequency_conf(&conf, tok.value, -1);
+						Frequency_execute(&conf);
+					}
+					else if(eNewEvent == PWMFREQUENCY){
+						Pwmfrequency_conf(&conf, tok.value, -1);
+						Pwmfrequency_execute(&conf);
+					}
+					else if(eNewEvent == AMPLITUDE){
+						Amplitude_conf(&conf, tok.value, tok.channel);	//misschien geen tok.channel, nog even naar kijken
+						Amplitude_execute(&conf);
+					}
+					else if(eNewEvent == PHASESHIFT){
+						Phaseshift_conf(&conf, tok.value, tok.channel);	
+						Phaseshift_execute(&conf);
+					}
+					else if(eNewEvent == PING){
+						Ping();		// deze functie nog maken (of ombouwen dat het klopt)
+					}
+					else if(eNewEvent == GATHER){
+						Gather_conf(&conf, samples);			// check of dit mogelijk is
+						Gather_execute(&conf);				// check of dit mogelijk is
+					}
+					else if(eNewEvent == INFO){
+						Info();		// deze functie nog maken (of ombouwen dat het klopt)
+					}
+					break;
+	
+				case Prepare_State:
+					if(eNewEvent == FREQUENCY){
+						Frequency_conf(&conf, tok.value, -1);
+					}
+					else if(eNewEvent == PWMFREQUENCY){
+						Pwmfrequency_conf(&conf, tok.value, -1);
+					}
+					else if(eNewEvent == AMPLITUDE){
+						Amplitude_conf(&conf, tok.value, tok.channel);	//misschien geen tok.channel, nog even naar kijken
+					}
+					else if(eNewEvent == PHASESHIFT){
+						Phaseshift_conf(&conf, tok.value, tok.channel);	
+					}
+					else if(eNewEvent == PING){
+						Ping();
+					}
+					else if(eNewEvent == GATHER){
+						Gather_conf(&conf, samples);		// check of dit mogelijk is
+					}
+					else if(eNewEvent == INFO){
+						Info();
+					}
+					else if(eNewEvent == EXECUTE){
+						eNextState = Execute();
+					}
+					break;
+				case Execute_State:
+					Frequency_execute(&conf);
+					Pwmfrequency_execute(&conf);
+					Phaseshift_execute(&conf);
+					Amplitude_execute(&conf);
+					Gather_execute(&conf);				// check of dit mogelijk is
+	
+					eNextState = Start();
+					break;
+				default:
+					eNextState = Stop();
+				}
+			}
+		}
+
 	}
 }
